c75ae69e09a8deb0542081fb29badbeb76d08b02
Michael Collins <mfcollins3@me.com>
2022-07-12 23:46:35 -0700

    feat: update Package.swift for libgit2 1.4.4

--- Package.swift
+++ Package.swift
@@ -21,22 +21,22 @@ let package = Package(
 		.binaryTarget(
 			name: "libgit2",
 			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/v1.4.3/libgit2.zip",
-			checksum: "d5d98de86bcdd13b41f457d025abadf20d8b358b88d9e8543d4f117ad863d015"
+			checksum: "e803a4a9d9a99940a6447d3182bedccbfb4765c334768c21c487febaaabe2d8d"
 		),
 		.binaryTarget(
 			name: "libssh2",
 			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/v1.4.3/libssh2.zip",
-			checksum: "43e0f5baf31bfecb5fd6bdd66b01d57e3582122a762eaab735311c6de4d09adb"
+			checksum: "6db15f16999b9cdb16a29a1b3c4b7b55a4ea787188c91eb1f0d95a0ac988cbb6"
 		),
 		.binaryTarget(
 			name: "libssl",
 			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/v1.4.3/libssl.zip",
-			checksum: "f0b23204497ba411da78ac4c1818badd9495e4e4c2ffef6babcbef05c697b0e1"
+			checksum: "c36bf18b53ccd4dc59e7f8941ae3a244261966cb04520551d800a02271142980"
 		),
 		.binaryTarget(
 			name: "libcrypto",
 			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/v1.4.3/libcrypto.zip",
-			checksum: "5cf22fa92138182dd34d15224d1f886aea949a7c89106261a309367abb2b6988"
+			checksum: "7de762979d28e6159b942216d27263a6f4dbb69e481ec25020d843368ed61da2"
 		),
 	]
 )
