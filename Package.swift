9eaeabee272822ff43f54073482522abf91fff7b
Michael Collins <mfcollins3@me.com>
2022-07-13 22:32:50 -0700

    feat: upgrade libgit2 to 1.5.0
    I upgraded libgit2 to 1.5.0. I rebuilt all of the libraries as
    XCFrameworks for distribution and consumption.

--- Package.swift
+++ Package.swift
@@ -20,23 +20,23 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "libgit2",
-			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/1.4.4/libgit2.zip",
-			checksum: "e803a4a9d9a99940a6447d3182bedccbfb4765c334768c21c487febaaabe2d8d"
+			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/1.5.0/libgit2.zip",
+			checksum: "cb06393e43fe89496fa154e661ecf5218c5fd483f6b719720b8b9c6a88f263a6"
 		),
 		.binaryTarget(
 			name: "libssh2",
-			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/1.4.4/libssh2.zip",
-			checksum: "6db15f16999b9cdb16a29a1b3c4b7b55a4ea787188c91eb1f0d95a0ac988cbb6"
+			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/1.5.0/libssh2.zip",
+			checksum: "c75b8a5202fd9f7cbe173a25507e853750f286a634f665692912f370581e4c00"
 		),
 		.binaryTarget(
 			name: "libssl",
-			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/1.4.4/libssl.zip",
-			checksum: "c36bf18b53ccd4dc59e7f8941ae3a244261966cb04520551d800a02271142980"
+			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/1.5.0/libssl.zip",
+			checksum: "130e5841e91f8bce647a1df75bef84a1014a54a885205ccf4a7ffc3ad42bc2d5"
 		),
 		.binaryTarget(
 			name: "libcrypto",
-			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/1.4.4/libcrypto.zip",
-			checksum: "7de762979d28e6159b942216d27263a6f4dbb69e481ec25020d843368ed61da2"
+			url: "https://github.com/mfcollins3/libgit2-ios/releases/download/1.5.0/libcrypto.zip",
+			checksum: "325a78943fb9bf3d8e9b02eb1aa00254ca575fb426be0c47c90324fcf4f37359"
 		),
 	]
 )
